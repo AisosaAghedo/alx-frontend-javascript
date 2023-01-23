eeda3dbdfd0661b90a25042cb0ad94e7e8e85757
Aisosa <allysonaghedo@gmail.com>
2023-01-23 23:45:22 +0100

    Object property value shorthand syntax

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    let income = income,
+    let gdp = gdp,
+    let capita = capita,
+  };
+
+  return budget;
+}
