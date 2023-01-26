9eb23d6de1927565eb744261e9b89086136dbc71
Aisosa Aghedo <allysonaghedo@gmail.com>
2023-01-26 22:11:05 +0000

    javscript classes OOP

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,4 +1,4 @@
-export default class Car {
+/*export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
@@ -7,5 +7,21 @@ export default class Car {
 
   cloneCar() {
     return new this.constructor(this._brand, this._motor, this._color);
+  }
+}*/
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
   }
 }
